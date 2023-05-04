fd40f01421c3c0b089c16b1faf9d6fcc9ab2e453
Carlosuca <00202118@uca.edu.sv>
2023-05-04 13:40:25 -0600

    Laboratorio07

--- Laboratorio05/app/src/main/java/com/cmpp/laboratorio05/ui/MainActivity.kt
+++ Laboratorio05/app/src/main/java/com/cmpp/laboratorio05/ui/MainActivity.kt
@@ -11,12 +11,11 @@ import com.cmpp.laboratorio05.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
 
-    //private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
 }
-
-//binding = ActivityMainBinding.inflate(inflater, container, false)
